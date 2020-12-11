a972bcc681088dcb6eea5fabc5673bdc0abe7014
SaanBitbucket <saanrashid@gmail.com>
2020-12-11 14:20:45 +0100

    page navigation with the link component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+// pages/index.jsx
 import React from 'react'
+import Link from 'next/link'
 
-export default () => <h1>Index Page</h1>
+export default () => (
+  <div>
+    <h1>Index page</h1>
+
+    <Link href="/notes">
+      <a>Notes</a>
+    </Link>
+  </div> 
+)
